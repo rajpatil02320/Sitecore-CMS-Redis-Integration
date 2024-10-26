29640528b19bfa7c59318d4ea575a96b08f6effd
Rajendra Patil <rajendra.patil090@gmail.com>
2024-10-27 04:15:46 +0530

    Add files via upload

---
+++ CacheHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Kernel;
+using SPJ.Foundation.Caching.Contracts;
+using SPJ.Foundation.Caching.Provider;
+
+namespace SPJ.Foundation.Caching
+{
+    public static class CacheHelper
+    {
+        public const int DefaultCacheDuration = 60;
+        static ICache _memoryCache = new InMemoryProvider("SPJ", 60);
+        static ICache _memoryLongCache = new InMemoryProvider("SPJ", Convert.ToInt32(Sitecore.Configuration.Settings.GetSetting("SPJ.LongCache.Duration", "10")));
+        static ICache _sharedRedisCache = (ICache)Sitecore.DependencyInjection.ServiceLocator.ServiceProvider.GetService(typeof(IRedisProvider));
+        static ICache _applicationCache = (ICache)Sitecore.DependencyInjection.ServiceLocator.ServiceProvider.GetService(typeof(ICache));
+
+
+        /// <summary>
+        /// The memory cache
+        /// </summary>
+        public static ICache MemoryCache => _memoryCache;
+
+        /// <summary>
+        /// The long memory cache
+        /// </summary>
+        public static ICache MemoryLongCache => _memoryLongCache;
+
+        /// <summary>
+        /// Gets the application cache.
+        /// </summary>
+        /// <value>
+        /// The application cache.
+        /// </value>
+        public static ICache ApplicationCache => _applicationCache;
+
+        public static ICache SharedRedisCache => _sharedRedisCache;
+
+        public static void Clear()
+        {
+            MemoryLongCache?.Clear();
+            MemoryCache?.Clear();
+            ApplicationCache?.Clear();
+        }
+    }
+}
---
+++ SessionCacheHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPJ.Foundation.Caching
+{
+    public static class SessionCacheHelper
+    {
+        /// <summary>
+        /// Get Session Value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T Get<T>(string key)
+        {
+            var session = HttpContext.Current?.Session;
+            if (null != session && session[key] != null)
+            {
+                return (T)session[key];
+            }
+
+            return default;
+        }
+        /// <summary>
+        /// Set Session Value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void Set<T>(string key, T value)
+        {
+            var session = HttpContext.Current?.Session;
+            if (null != session && value != null)
+            {
+                session[key] = value;
+            }
+        }
+
+        public static void Remove(string key)
+        {
+            var session = HttpContext.Current?.Session;
+            if (null != session && session[key] != null)
+            {
+                session[key] = null;
+            }
+        }
+    }
+}
